0979b81c42b1bebf15a0f31d71f23e91ba0f8fc0
Jeon <starhyunee@naver.com>
2022-03-09 20:29:52 +0900

    second commit

--- Project6/sorting.cpp
+++ Project6/sorting.cpp
@@ -16,3 +16,50 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     }
     return answer;
 }
+
+/* 프로그래머스 고득점 kit - 정렬 - 가장 큰 수 */
+bool compare(int a, int b) {
+    string str_a = to_string(a) + to_string(b);
+    string str_b = to_string(b) + to_string(a);
+    return str_a > str_b;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    sort(numbers.begin(), numbers.end(), compare);
+    if (numbers[0] == 0) {
+        return "0";
+    }
+    for (int i = 0; i < numbers.size(); i++) {
+        answer = answer + to_string(numbers[i]);
+    }
+    return answer;
+}
+
+
+/* 프로그래머스 고득점 kit - 정렬 - h index */
+int solution1(vector<int> citations) {
+    int answer = 0;
+    int count = 0;
+    sort(citations.begin(), citations.end());
+    int size = citations.size();
+    if (citations[size - 1] == 0)
+        return 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (citations[j] >= answer) {
+                count++;
+            }
+        }
+        if (count > answer) {
+            answer++;
+            count = 0;
+        }
+        else {
+            if (answer == count + 1)
+                return answer - 1;
+            return answer;
+        }
+    }
+}
+
